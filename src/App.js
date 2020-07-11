78fb4e01ba0aa7f5de487d7b6539f6b5b495d784
gal <anlaganlag>
2020-07-11 11:32:50 +0800

    列表展示

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const title = 'React';
+const welcome = {
+    greeting:"Hey",
+    title:"React",
 }
 
+const list = [
+  {
+    title:"React",
+    url:"https://reactjs.org/",
+    author:"Jordan Walk",
+    num_commnets:3,
+    points:4,
+    objectID:0,
+  },
+  {
+    title:"Redux",
+    url:"https://redux.js.org/",
+    author:"Dan Abramov,Andrew Clark",
+    num_commnets:2,
+    points:5,
+    objectID:1,
+  },
+];
+
+function getTitle(title){
+  return title;
+}
+function App() {
+  return (
+  <div>
+    <h1>
+      My Coding Exprience: {getTitle("GalFond")}
+    </h1>
+    
+    <label htmlFor="search">Search it: </label>
+    <input id="search" type="text" />
+    <hr />
+
+    {list.map(
+      item=>
+      <div key={item.objectID}> 
+      <span><a href={item.url}>{item.title} </a></span>
+      <span>{item.author} </span>
+      <span>{item.num_commnets} </span>
+      <span>{item.points}</span>
+      </div>
+    )}
+  </div>
+  )
+}
 export default App;
